0b883ed821ec2b664f8ef95b4a193f3c69de5710
“santhoshi123654” <santhoshi.badam@gmail.com>
2021-12-20 22:02:10 +0530

    Added:CabService With MultipleRides Program.

---
+++ src/main/java/com/bridgelabz/CabInvoiceGenerators/CabServiceTest.java
@@ -0,0 +1,43 @@
+package com.bridgelabz.CabInvoiceGenerators;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CabServiceTest {
+	
+	CabService cabService = null;
+	
+	@Before
+    public void setUp() throws Exception {
+    	cabService = new CabService();
+    }
+    
+    @Test
+    public void calculateFare_should_return_totalFare() {
+        double distance = 4.0;
+        int time = 10;
+        double totalFare = cabService.calculateFare(distance, time);
+
+        Assert.assertEquals(50.0,totalFare,0.0);
+    }
+
+    @Test
+    public void calculateFare_should_return_Minimum_Fare() {
+        double distance = 0.25;
+        int time = 2;
+        double totalFare = cabService.calculateFare(distance, time);
+
+        Assert.assertEquals(5.0,totalFare,0.0);
+    }
+	
+    @Test
+    public void givenMultipleRides_ShouldReturnTotalAggregate() {
+        Ride[] rides = {
+                new Ride(2.0, 5),
+                new Ride(0.1, 1)};
+        double totalAggregate = cabService.calculateFare(rides);
+
+        Assert.assertEquals(30.0,totalAggregate,0.0);
+    }
+}
